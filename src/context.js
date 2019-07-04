ab58f85757f03652e0843b295ae31c623f072059
ciricstefan <ciric.stefan95@gmail.com>
2019-07-04 17:23:00 +0200

    create context state and methods, use it in ProductList

--- src/context.js
+++ src/context.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import { storeProducts, detailProduct } from "./data";
 
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct: detailProduct
+  };
+
+  handleDetail = () => {
+    console.log("hello from detail");
+  };
+
+  addToCart = () => {
+    console.log("add To Cart");
+  };
+
   render() {
     return (
-      <ProductContext.Provider value="Hello from context">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
