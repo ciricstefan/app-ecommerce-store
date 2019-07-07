f08386dc89dd9a465980b1fc90cda776112fba4c
ciricstefan <ciric.stefan95@gmail.com>
2019-07-07 20:20:03 +0200

    create modal product functionality

--- src/context.js
+++ src/context.js
@@ -7,13 +7,16 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: []
+    cart: [],
+    modalOpen: true,
+    modalProduct: detailProduct
   };
 
   componentDidMount() {
     this.setProducts();
   }
 
+  // get placeholder phone database
   setProducts = () => {
     let tempProducts = [];
     storeProducts.forEach(item => {
@@ -25,10 +28,12 @@ class ProductProvider extends Component {
     });
   };
 
+  // find product depending on id(reusable method)
   getItem = id => {
     return this.state.products.find(item => item.id === id);
   };
 
+  // get product for details page
   handleDetail = id => {
     const product = this.getItem(id);
     this.setState(() => {
@@ -36,6 +41,7 @@ class ProductProvider extends Component {
     });
   };
 
+  // add to cart button functionality
   addToCart = id => {
     let tempProducts = [...this.state.products];
     const index = tempProducts.indexOf(this.getItem(id));
@@ -49,13 +55,30 @@ class ProductProvider extends Component {
     });
   };
 
+  // functionality to open clicked product modal
+  openModal = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modalOpen: true };
+    });
+  };
+
+  // functionality to close clicked product modal
+  closeModal = () => {
+    this.setState(() => {
+      return { modalOpen: false };
+    });
+  };
+
   render() {
     return (
       <ProductContext.Provider
         value={{
           ...this.state,
           handleDetail: this.handleDetail,
-          addToCart: this.addToCart
+          addToCart: this.addToCart,
+          openModal: this.openModal,
+          closeModal: this.closeModal
         }}
       >
         {this.props.children}
