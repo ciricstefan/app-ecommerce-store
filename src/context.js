ea2ce7eb2f87071945040dbceced3d88f1688154
ciricstefan <ciric.stefan95@gmail.com>
2019-07-10 21:44:27 +0200

    create method and state placeholders for Cart component

--- src/context.js
+++ src/context.js
@@ -9,7 +9,10 @@ class ProductProvider extends Component {
     detailProduct: detailProduct,
     cart: [],
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
 
   componentDidMount() {
@@ -70,6 +73,26 @@ class ProductProvider extends Component {
     });
   };
 
+  // increment method for cart items
+  increment = id => {
+    console.log("this is increment method");
+  };
+
+  // decrement method for cart items
+  decrement = id => {
+    console.log("this is decrement method");
+  };
+
+  // method to remove specific item from cart
+  removeItem = id => {
+    console.log("item removed");
+  };
+
+  // method to clear whole cart
+  clearCart = () => {
+    console.log("cart was cleared");
+  };
+
   render() {
     return (
       <ProductContext.Provider
@@ -78,7 +101,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {this.props.children}
