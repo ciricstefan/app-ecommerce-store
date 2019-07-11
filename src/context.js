5f3ba7fd4d6db646af225252547e1fa16737be5c
ciricstefan <ciric.stefan95@gmail.com>
2019-07-12 01:03:55 +0200

    finish increment method

--- src/context.js
+++ src/context.js
@@ -80,7 +80,20 @@ class ProductProvider extends Component {
 
   // increment method for cart items
   increment = id => {
-    console.log("this is increment method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count + 1;
+    product.total = product.price * product.count;
+    this.setState(
+      () => {
+        return { cart: [...tempCart] };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
 
   // decrement method for cart items
